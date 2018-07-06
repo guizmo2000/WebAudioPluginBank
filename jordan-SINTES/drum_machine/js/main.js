2c52edc8306dafeec4bd679d6de8f6a173f8e3f3
jordanSI06 <jordan.sintes@gmail.com>
2018-07-06 14:42:59 +0200

    Import Drum Machine

---
+++ jordan-SINTES/drum_machine/js/main.js
@@ -0,0 +1,6 @@
+/*  ################################## WAH ########################################  */
+
+/* ES6 web audio class following the API standard
+* Author : Jordan Sintes
+* Comment: Scheduling web audio with precisions: https://www.html5rocks.com/en/tutorials/audio/scheduling/
+*/
