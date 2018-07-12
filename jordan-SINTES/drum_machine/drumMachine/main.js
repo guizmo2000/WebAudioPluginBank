50bcd2dd6fe204da387e5d54e6b9452619f88bbd
jordanSI06 <jordan.sintes@gmail.com>
2018-07-12 11:30:26 +0200

    Initializing main js var + correction wah

--- jordan-SINTES/drum_machine/drumMachine/main.js
+++ jordan-SINTES/drum_machine/drumMachine/main.js
@@ -10,18 +10,117 @@ window.DrumMachine = class DrumMachine extends WebAudioPluginCompositeNode {
         super(ctx, options)
         this.state;
 
-        this.addParam({ 
-            name: 'kickPitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'snarePitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'hihatPitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'tom1PitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'tom2PitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'tom3PitchVal', defaultValue: 0.5, minValue: 0, maxValue:1,
-            name: 'swingFactor', defaultValue: 0.5, minValue:0, maxValue:1,
+        this.addParam({
+			 
         });
 
         this.params = {
-            effectDry
+			timeWorker : null,
+			lastDrawTime : -1,
+			 kNumInstruments : 6,
+			 kInitialKitIndex: 10,
+			 kMaxSwing: .08,
+
+			 beatReset : {
+							 "kitIndex":0,
+							 "tempo":100,
+							 "swingFactor":0,
+							 "kickPitchVal":0.5,
+							 "snarePitchVal":0.5,
+							 "hihatPitchVal":0.5,
+							 "tom1PitchVal":0.5,
+							 "tom2PitchVal":0.5,
+							 "tom3PitchVal":0.5,
+							 "rhythm1":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							 "rhythm2":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							 "rhythm3":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							 "rhythm4":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							 "rhythm5":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							 "rhythm6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+						},
+			beatInitial : {
+							"kitIndex":11,
+							"tempo":100,
+							"swingFactor":0,
+							"kickPitchVal":0.46478873239436624,
+							"snarePitchVal":0.45070422535211263,
+							"hihatPitchVal":0.15492957746478875,
+							"tom1PitchVal":0.7183098591549295,
+							"tom2PitchVal":0.704225352112676,
+							"tom3PitchVal":0.8028169014084507,
+							"rhythm1":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							"rhythm2":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							"rhythm3":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							"rhythm4":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							"rhythm5":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+							"rhythm6":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+						},
+			theBeat : cloneBeat(beatReset),
+
+			kickPitch : 0,
+			snarePitch : 0,
+			hihatPitch : 0,
+			tom1Pitch : 0,
+			tom2Pitch : 0,
+			tom3Pitch : 0,
+
+			mouseCapture : null,
+			mouseCaptureOffset =0,
+
+			loopLength : 16,
+			rhythmIndex : 0,
+			kMinTempo : 52,
+			kMaxTempo : 180,
+			noteTime : 0.0,
+
+			instruments : ['Kick', 'Snare', 'HiHat', 'Tom1', 'Tom2', 'Tom3'],
+			volumes : [0, 0.3, 1],
+			kitName : [
+				"R8",
+				"CR78",
+				"KPR77",
+				"LINN",
+				"Kit3",
+				"Kit8",
+				"Techno",
+				"Stark",
+				"breakbeat8",
+				"breakbeat9",
+				"breakbeat13",
+				"acoustic-kit",
+				"4OP-FM",
+				"TheCheebacabra1",
+				"TheCheebacabra2"
+			],
+			kitNamePretty : [
+				"Roland R-8",
+    			"Roland CR-78",
+    			"Korg KPR-77",
+    			"LinnDrum",
+    			"Kit 3",
+    			"Kit 8",
+    			"Techno",
+    			"Stark",
+    			"Breakbeat 8",
+    			"Breakbeat 9",
+    			"Breakbeat 13",
+    			"Acoustic Kit",
+    			"4OP-FM",
+    			"The Cheebacabra 1",
+    			"The Cheebacabra 2"
+			],
+
+			decodedFunctions : [
+				function (buffer) { this.kickBuffer = buffer; },
+				function (buffer) { this.snareBuffer = buffer; },
+				function (buffer) { this.hihatBuffer = buffer; },
+				function (buffer) { this.tom1 = buffer; },
+				function (buffer) { this.tom2 = buffer; },
+				function (buffer) { this.tom3 = buffer; }
+			],
+
+
+
         }
         this.setup();   
     }
