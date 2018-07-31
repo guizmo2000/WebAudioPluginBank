394d85dd8a1246e69a251c7f4fb34b517a363df9
Guillaume Etevenard <guillaume.etev@gmail.com>
2018-07-31 09:41:39 +0200

    clean the clean machine

--- Mike-AUBENAS/MixingConsole/Mixer/main.js
+++ Mike-AUBENAS/MixingConsole/Mixer/main.js
@@ -1,44 +1,34 @@
 //jslint:disable:one-line no-trailing-spaces
 
-window.Mixer = class Mixer extends WebAudioPluginCompositeNode
-{
-	constructor(ctx,URL ,options)
-	{
-		super(ctx,URL,options)
+window.Mixer = class Mixer extends WebAudioPluginCompositeNode {
+	constructor(ctx, URL, options) {
+		super(ctx, URL, options)
 
 		this.state;
 		this.urlChannel = "https://wasabi.i3s.unice.fr/WebAudioPluginBank/Mike-AUBENAS/MixingConsole/ChannelMixer";
 		this._numberOfInputs = 2;
+		parent = this;
 
-		if(options)
+		if (options)
 			this.arrayNodeToConnect = options.arrayNodeToConnect ? options.arrayNodeToConnect : 'no nodes';
 		else
 			this.arrayNodeToConnect = 'no nodes';
 
 		this._metadata =
-		{
-			"name": "wasabi-Mixer",
-			"version": 1,
-			"category": "Mixer",
-			"type": "Audio",
-			"description": "A channel part of a mixer",
-			"thumbnailImage": "https://...",
-			"URLs": "https://.../doc",
-			"authorInformation": "Mike AUBENAS, i3s intern in Nice - Sophia-Antipolis, France"
-		}
-
-		this.params =
-		{
-			"gain":
 			{
-				"value": 1,
-				"range":
-				{
-					"min": 0,
-					"max": 10
-				}
+				"name": "wasabi-Mixer",
+				"version": 1,
+				"category": "Mixer",
+				"type": "Audio",
+				"description": "A channel part of a mixer",
+				"thumbnailImage": "https://...",
+				"URLs": "https://.../doc",
+				"authorInformation": "Mike AUBENAS, i3s intern in Nice - Sophia-Antipolis, France"
 			}
-		};
+
+		this.addParam({ name: 'gain', defaultValue: 1, minValue: 0, maxValue: 10 });
+		this.addParam({ name: 'nbcanaux', defaultValue: 2, minValue: 2, maxValue: 6 });
+
 
 
 		this.getNumberOfChannels();
@@ -47,82 +37,72 @@ window.Mixer = class Mixer extends WebAudioPluginCompositeNode
 	}
 
 	get numberOfInputs() {
-    return this._numberOfInputs;
+		return this._numberOfInputs;
 	}
 	set numberOfInputs(num) {
-    this._numberOfInputs =  num;
+		this._numberOfInputs = num;
 	}
 
 	get numberOfOutputs() {
-    return 1;
-	}
-	
-	get dynamicParam(){
-		return {listento:"numberOfInputs", description:"inputs number increase when pushing '+' on GUI"}
+		return 1;
 	}
 
-	getNumberOfChannels()
-	{
-		if(this.arrayNodeToConnect != 'no nodes')
+	get dynamicParam() {
+		return { listento: "numberOfInputs", description: "inputs number increase when pushing '+' on GUI" }
+	}
+
+	getNumberOfChannels() {
+		if (this.arrayNodeToConnect != 'no nodes')
 			return this.arrayNodeToConnect.length;
 	}
 
 
-  setParam(key, value) {
-    //console.log(key, value);
-    try {
-      this[key] = (value);
-    } catch (error) {
+	setParam(key, value) {
+		console.log(key, value);
+		try {
+			this[key] = (value);
+		} catch (error) {
 
-      console.warn("this plugin does not implement this param")
-    }
-  }
+			console.log(error);
+		}
+	}
 
-	getArrayNodeToConnect()
-	{ return this.arrayNodeToConnect; }
+	getArrayNodeToConnect() { return this.arrayNodeToConnect; }
 
-	setArrayNodeToConnect(arrayMedia)
-	{  this.arrayNodeToConnect = arrayMedia; }
+	setArrayNodeToConnect(arrayMedia) { this.arrayNodeToConnect = arrayMedia; }
 
 
-	
-
-	setup()
-	{
+	setup() {
 		this.createNode();
 	}
 
-	createNode()
-	{
+	createNode() {
 		this.master = this.context.createGain();
 		this.master.connect(this._output);
-		for (let i=1;i<this.numberOfInputs;i++){
-			let numchannel ="InputForchannel"+this.inputs.length+1;
+		for (let i = 1; i < this.numberOfInputs; i++) {
+			let numchannel = "InputForchannel" + this.inputs.length + 1;
 			this[numchannel] = this.context.createGain();
 			this.inputs.push(this[numchannel]);
 		}
-		
+
 	}
 
 
-	addChannel()
-	{
-		let numchannel ="InputForchannel"+this.inputs.length+1;
+	addChannel() {
+		let numchannel = "InputForchannel" + this.inputs.length + 1;
 		this[numchannel] = this.context.createGain();
 		this.inputs.push(this[numchannel]);
 
-		var plugin = new window.WasabiChannelMixer(this.context, this.urlChannel, {"channelNumber" : this.inputs.length});
+		var plugin = new window.WasabiChannelMixer(this.context, this.urlChannel, { "channelNumber": this.inputs.length });
 
-		plugin.load().then((node) =>
-		{
-			plugin.loadGui().then((elem) =>
-			{
+		plugin.load().then((node) => {
+			plugin.loadGui().then((elem) => {
 				this.gui._root.querySelector('#arrayOfChannels').appendChild(elem);
-				this.numberOfInputs ++;
+				this.numberOfInputs++;
 				this.gui.setWidth(this.gui.properties.dataWidth.value + elem.properties.dataWidth.value);
 				var event = new Event('change');
-						// Dispatch it.
-				this.gui.dispatchEvent(event); 
+				// Dispatch it.
+				this.gui.dispatchEvent(event);
 			});
 
 			this[numchannel].connect(node);
@@ -131,28 +111,41 @@ window.Mixer = class Mixer extends WebAudioPluginCompositeNode
 		});
 	}
 
-	changeMasterGain(value)
-	{
-		if ((value >= this.params.gain.range.min) && (value <= this.params.gain.range.max))
-		{
+	changeMasterGain(value) {
+		if ((value >= this.params.gain.minValue) && (value <= this.params.gain.maxValue)) {
 			this.params.gain.value = value;
-			this.master.gain.setValueAtTime( parseFloat(value, 10), this.context.currentTime );
+			this.master.gain.setValueAtTime(parseFloat(value, 10), this.context.currentTime);
 		}
 	}
 
+	set gain(value) {
+		this.params.gain = value;
+		this.master.gain.setValueAtTime(value, this.context.currentTime);
+	}
+
+	set nbcanaux(nb) {
+		console.log("yes");
+		this.params.nbcanaux = nb;
+	//	let diff = nb - this.numberOfInputs;
+		//this.addChannel();
+		//this.addChannel();
+		// if (diff != 0) {
+		// 	(async function loop() {
+		// 		for (let j = 0; j < diff; j++) { await parent.addChannel() }
+		// 	})();
+		// }
+	}
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-window.WasabiMixer = class WasabiMixer extends WebAudioPluginFactory
-{
-	constructor(context, baseUrl, options)
-	{
-		super(context, baseUrl,options);
+window.WasabiMixer = class WasabiMixer extends WebAudioPluginFactory {
+	constructor(context, baseUrl, options) {
+		super(context, baseUrl, options);
 	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-AudioContext.prototype.createWasabiDelayCompositeNode = OfflineAudioContext.prototype.createWasabiDelayCompositeNode = function (options)
-{ return new Mixer(this,baseUrl, options); };
+AudioContext.prototype.createWasabiDelayCompositeNode = OfflineAudioContext.prototype.createWasabiDelayCompositeNode = function (options) { return new Mixer(this, baseUrl, options); };
