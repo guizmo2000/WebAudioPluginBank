a22b1cca1e08b53de22e4528fc84fed410b54f5a
Jordan SINTES <jordan.sintes@gmail.com>
2018-11-16 15:38:55 +0100

    Test

--- jordan-SINTES/drum_machine/drumMachine/main.js
+++ jordan-SINTES/drum_machine/drumMachine/main.js
@@ -130,6 +130,7 @@ window.DrumMachine = class DrumMachine extends WebAudioPluginCompositeNode {
 
 		}
 		this.setup();
+		this.loadBeat(params);
 	}
 	/*    ################     API METHODS    ###############   */
 	// p9 count inputs
