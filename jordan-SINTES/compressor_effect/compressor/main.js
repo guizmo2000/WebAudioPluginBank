bf73129721f52f7d664b32604d552407b14b797d
Jordan SINTES <jordan.sintes@gmail.com>
2019-01-17 11:44:14 +0100

    Initializing project

---
+++ jordan-SINTES/compressor_effect/compressor/main.js
@@ -0,0 +1,59 @@
+/*  ################################## WAH ########################################  */
+
+/* ES6 web audio class following the API standard
+* Author : Jordan Sintes
+* Comment: Based on the structure of Pedal Wah Vox V847, more information: https://www.electrosmash.com/vox-v847-analysis
+*/
+window.Compressor = class Compressor extends WebAudioPluginCompositeNode {
+	constructor(ctx, URL, options) {
+		super(ctx, URL, options)
+		/*    ################     API PROPERTIES    ###############   */
+
+		this.state;
+		this.params = {
+			
+		}
+
+		super.setup();
+	}
+
+	/*    ################     API METHODS    ###############   */
+
+	getPatch(index) {
+		console.warn("this module does not implements patches use getState / setState to get an array of current params values ");
+	}
+	setPatch(data, index) {
+		console.warn("this module does not implements patches use getState / setState to get an array of current params values ");
+	}
+
+	setParam(key, value) {
+		console.log(key, value);
+		try {
+			this[key] = (value);
+		} catch (error) {
+			console.log(key, error)
+			console.warn("this plugin does not implement this param")
+		}
+	}
+
+	createNodes() {
+		
+	}
+
+	connectNodes() {
+		
+	}
+
+	/*  #########  Personnal code for the web audio graph  #########   */
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+window.WasabiCompressor = class WasabiCompressor extends WebAudioPluginFactory {
+	constructor(context, baseUrl) { super(context, baseUrl); }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+AudioContext.prototype.createWasabiDelayCompositeNode = OfflineAudioContext.prototype.createWasabiDelayCompositeNode = function (options) { return new Compressor(this, options); };
+
