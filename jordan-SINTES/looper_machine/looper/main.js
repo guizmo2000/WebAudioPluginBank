549232f70c944b9bae14a3747d3758ff5e7d5918
Jordan SINTES <jordan.sintes@gmail.com>
2019-03-26 14:23:27 +0100

    Update main.js

--- jordan-SINTES/looper_machine/looper/main.js
+++ jordan-SINTES/looper_machine/looper/main.js
@@ -11,7 +11,7 @@ window.LooperMachine = class LooperMachine extends WebAudioPluginCompositeNode {
 
         this.options = options;
         this.state;
-        this.params = { "status": "unavaiable" }
+        this.params = { "status": "unavaiable", "mix":50 }
         super.setup();
 
     }
@@ -46,25 +46,32 @@ window.LooperMachine = class LooperMachine extends WebAudioPluginCompositeNode {
 
     set status(_status) {
         var mediaRecorder;
+        if (navigator.mediaDevices) {
+            console.log('getUserMedia supported');
+            var constraints = { audio: true };
+            var chunks = [];
+            navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+                mediaRecorder = new MediaRecorder(stream);
+                if(_status === "record"){
+                   
+                }
+                mediaRecorder.start();
+                console.log(mediaRecorder.state);
+                  console.log("recorder started");
+                
+                });
+        }
         if (_status === "record") {
-            if (navigator.mediaDevices) {
-                console.log('getUserMedia supported');
-                var constraints = { audio: true };
-                var chunks = [];
-                navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-                    mediaRecorder = new MediaRecorder(stream);
-                    mediaRecorder.start();
-                    console.log(mediaRecorder.state);
-                      console.log("recorder started");
-                        
-                    });
-            }
         } else if (_status === "play") {
             mediaRecorder.stop();
             console.log(mediaRecorder);
         } else if (_status === "stop") {
 
         }
+    }
+
+    set mix(_mix){
+        this.params.mix=_mix;
     }
 }
 
